c9e982e1da1d9fc1137da6868b837b1914bd8a54
Tomaz Zaman <tomaz@optiss.si>
2012-03-25 14:06:28 +0200

    Submission of topics added

--- app.js
+++ app.js
@@ -28,7 +28,12 @@ app.configure('production', function(){
 	app.use(express.errorHandler()); 
 });
 
-app.dynamicHelpers({
+
+app.all('*', function(req, res, next) {
+	next();
+});
+
+app.dynamicHelpers({	
 	user: function(req, res) {
 		return req.user;
 	},
@@ -70,27 +75,85 @@ app.get('/', function(req, res) {
 });
 
 app.get('/topic/:title', function(req, res) {
-	var id = req.params.title.substr(0,req.params.title.indexOf('-'));
+	var id = req.params.title.substr(0,req.params.title.indexOf('-')) || 0;
+	if (id === 0) { return res.redirect('/'); }
 	db.getSingle(id, function(error, results) {
-		var topic = results[0]
+		if (results.length === 0) { return res.redirect('/'); }
+		var topic = results[0];
+		var pros = [];
+		var cons = [];
+		var count = 0;
+		var pro_percent = 0;
+		var con_percent = 0;
+		results.forEach(function(result){
+			if(result.pro !== null) {
+				if(result.pro === 1) {
+					pros.push(result);
+				} else {
+					cons.push(result);
+				}
+			}
+			count++;
+		});
+		if(pros.length === 0 && cons.length === 0) {
+			pro_percent = 0;
+			con_percent = 0;
+		} else {
+			pro_percent = parseInt(pros.length / count  * 100);	
+			con_percent = 100 - pro_percent
+		}
 		res.render('topic.html', {
 			title: 'Decide it! :: ' + topic['title'],
-			topic: topic
+			topic: topic,
+			pros: pros,
+			cons: cons,
+			pro_percent: pro_percent,
+			con_percent: con_percent
 		});		
 	});
 });
 
 app.post('/topic/:title', auth.ensureAuthenticated, function(req, res) {
 	var id = req.params.title.substr(0,req.params.title.indexOf('-'));
-	console.log(req.body.opinion_pro);
-	console.log(req.body.opinion_con);	
-	res.redirect(req.params.title);
+	if(req.body.opinion_pro != undefined) {
+		if(req.body.opinion_pro.length > 2) {
+			db.insertOpinion(req.body.opinion_pro, req.user.id, id, '1');
+		}
+	}
+	if(req.body.opinion_con != undefined) {
+		if(req.body.opinion_con.length > 2) {
+			db.insertOpinion(req.body.opinion_con, req.user.id, id, '0');
+		}
+	}
+	res.redirect('/topic/' + req.params.title);
 });
 
 app.get('/topic', function(req, res, next) {
 	res.redirect('/');
 });
 
+/* SUBMIT */
+app.get('/submit', auth.ensureAuthenticated, function(req, res) {
+	res.render('submit.html', {
+		title: 'Decide it! :: Submit a topic',
+	});
+});
+app.post('/submit', auth.ensureAuthenticated, function(req, res) {
+	if(req.body.title.length < 20 || req.body.text.length < 20) {
+		return res.render('submit.html', {
+			title: 'Decide it! :: Submit a topic',
+			topic: req.body.title,
+			text: req.body.text,
+			error: true
+		});
+	}
+	
+	db.insertTopic(req.body.title, req.body.text, req.user.id);
+	return res.redirect('/');
+});
+/* /SUBMIT */
+
+/* LOGIN */
 app.get('/login', function(req, res) {
 	res.render('login.html', {
 		title: 'Decide it! :: Login',
@@ -105,6 +168,7 @@ app.get('/logout', function(req, res){
 	req.logout();
 	res.redirect('/');
 });
+/* /LOGIN */
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
